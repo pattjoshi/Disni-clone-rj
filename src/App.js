50be832700e1011ccc57406e57ba96e0910763fd
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-06-03 13:03:32 +0530

    Login page is complited and all current date Dinamicaly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 const App = () => {
-  return <div>Hello from app</div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
