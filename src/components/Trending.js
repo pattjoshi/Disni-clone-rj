1798483ace6adc61bf5a12bf5d49a9314893cb93
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-06-06 22:07:35 +0530

    adding data from firebase show dinamiclly

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,32 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { selectTrending } from "../feachers/movie/movieSlice";
 
 const Trending = () => {
+  const movies = useSelector(selectTrending);
+  //console.log(movies, ":🛢️");
   return (
     <Container>
-      <h4>Trending</h4>
+      <h4>Disney</h4>
       <Content>
-        <Wrap>
-          <Link to="/">
-            <img src="/images/slider-badging.jpg" alt="Recomedent" />
-          </Link>
-        </Wrap>
-        <Wrap>
-          <Link to="/">
-            <img src="/images/slider-badging.jpg" alt="Recomedent" />
-          </Link>
-        </Wrap>
-        <Wrap>
-          <Link to="/">
-            <img src="/images/slider-badging.jpg" alt="Recomedent" />
-          </Link>
-        </Wrap>
-        <Wrap>
-          <Link to="/">
-            <img src="/images/slider-badging.jpg" alt="Recomedent" />
-          </Link>
-        </Wrap>
+        {movies &&
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              {movie.id}
+              <Link to={`/detail/` + movie.id}>
+                <img src={movie.cardImg} alt={movie.title} />
+              </Link>
+            </Wrap>
+          ))}
       </Content>
     </Container>
   );
