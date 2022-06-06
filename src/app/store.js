cddeb0fd5b0d6afdb3ea139ce4bff2653127ac7f
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-06-06 20:48:35 +0530

    Adding data in-to firebase store and get in redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import movieReducer from "../feachers/movie/movieSlice";
 import userReducer from "../feachers/users/userSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import { setMovies } from "../feachers/movie/movieSlice";
+import { selectUserName } from "../feachers/users/userSlice";
+import db from "../firebase";
 import ImgSlider from "./ImgSlider";
 import NewDisney from "./NewDisney";
 import Originals from "./Originals";
@@ -8,6 +12,47 @@ import Trending from "./Trending";
 import Viewers from "./Viewers";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    console.log("hello");
+    db.collection("movies").onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        console.log(recommends);
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "trending":
+            trending = [...trending, { id: doc.id, ...doc.data() }];
+            break;
+        }
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [userName]);
   return (
     <Container>
       <ImgSlider />
